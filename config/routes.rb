394c65860fe3f2b6f8c7766b93688efb0a2ffcb8
cxrlospxndo <cxrlospxndo@gmail.com>
2012-06-06 00:38:51 -0500

    tex_field pswd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Step::Application.routes.draw do
   resources :users
-
+  
+  # match ':name' => 'Comments#index', :as => 'user_comments'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
