e25d4458f68ba5486ee1f01597087b97531c501a
cxrlospxndo <cxrlospxndo@gmail.com>
2013-07-21 13:15:00 -0500

    fb not working

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-	ENV['facebook-id'] = "272946046069969"
-	ENV['facebook-secret'] = "7061d23e054ae2a04c7b3d0faa1604e0"
+	ENV['facebook-id'] = "250953895009540"
+	ENV['facebook-secret'] = "00915a1dd3b4003490d3eeb3932df1a4"
 else
 	ENV['facebook-id'] = "478281898863951"
 	ENV['facebook-secret'] = "f7885011357c9e69d0b0b20b4bea4a63"
