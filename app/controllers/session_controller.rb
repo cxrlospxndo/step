02381987c6cde84b5c54793a41072a42a90bf8d6
cxrlospxndo <cxrlospxndo@gmail.com>
2013-08-10 14:43:00 -0500

    xls not working on some devices :sob:

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -12,8 +12,8 @@ class SessionController < ApplicationController
   def show
     @notas = Uni.notas session[:codigo], session[:password]
     redirect_to(:action => 'create') unless @notas
-
     @data = Uni.data session[:codigo]
+
     respond_to do |format|
       format.html
       format.xls { send_data (ApplicationHelper.to_csv(@data, @notas, "\t")),
@@ -22,6 +22,7 @@ class SessionController < ApplicationController
                              disposition: "attachment"}
       format.pdf do
         pdf = SessionPdf.new(@data, @notas)
+        # cookies are not supported on some devices
         send_data pdf.render, filename: "#{session[:codigo]}.pdf",
                   type: "application/pdf",
                   disposition: "attachment"
